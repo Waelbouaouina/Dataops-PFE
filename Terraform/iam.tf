2c3d41a1b1657cadd590518a03f0228bd6d16ac0
Waelbouaouina <bouaouina.wael@esprit.tn>
2025-07-02 11:30:55 +0200

    IAM tf

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -4,7 +4,7 @@
 data "google_project" "current" {}
 
 ##############################
-# Création du Service Account
+# Création du Service Account dataloader-sa
 ##############################
 resource "google_service_account" "dataloader_sa" {
   account_id   = "dataloader-sa"
@@ -13,22 +13,15 @@ resource "google_service_account" "dataloader_sa" {
 
 ##############################
 # IAM bindings pour dataloader-sa
+# -> BIGQUERY : création de tables
+# -> STORAGE : lecture d’objets
+# -> PUBSUB  : publication
+# -> COMPOSER Service Agent Extension
 ##############################
-resource "google_project_iam_member" "sa_pubsub_publisher" {
-  project = var.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${google_service_account.dataloader_sa.email}"
-}
 
-resource "google_service_account_iam_member" "dataloader_sa_act_as_composer" {
-  service_account_id = google_service_account.dataloader_sa.name
-  role               = "roles/iam.serviceAccountUser"
-  member             = "serviceAccount:service-${data.google_project.current.number}@cloudcomposer-accounts.iam.gserviceaccount.com"
-}
-
-resource "google_project_iam_member" "sa_bigquery_data_editor" {
+resource "google_project_iam_member" "sa_bigquery_data_owner" {
   project = var.project_id
-  role    = "roles/bigquery.dataEditor"
+  role    = "roles/bigquery.dataOwner"
   member  = "serviceAccount:${google_service_account.dataloader_sa.email}"
 }
 
@@ -38,65 +31,71 @@ resource "google_project_iam_member" "sa_storage_object_viewer" {
   member  = "serviceAccount:${google_service_account.dataloader_sa.email}"
 }
 
-##############################
-# IAM bindings pour TON Terraform
-##############################
-resource "google_project_iam_member" "terraform_bigquery_admin" {
+resource "google_project_iam_member" "sa_pubsub_publisher" {
   project = var.project_id
-  role    = "roles/bigquery.admin"
-  member  = "user:${var.terraform_sa_email}"
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${google_service_account.dataloader_sa.email}"
 }
 
-resource "google_project_iam_member" "terraform_storage_admin" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-  member  = "user:${var.terraform_sa_email}"
-}
-
-resource "google_project_iam_member" "terraform_cloudfunctions_admin" {
-  project = var.project_id
-  role    = "roles/cloudfunctions.admin"
-  member  = "user:${var.terraform_sa_email}"
-}
-
-resource "google_project_iam_member" "terraform_run_admin" {
-  project = var.project_id
-  role    = "roles/run.admin"
-  member  = "user:${var.terraform_sa_email}"
-}
-
-resource "google_project_iam_member" "terraform_pubsub_admin" {
-  project = var.project_id
-  role    = "roles/pubsub.admin"
-  member  = "user:${var.terraform_sa_email}"
-}
-
-##############################
-# Extension Composer Service Agent
-##############################
 resource "google_project_iam_member" "composer_service_agent_ext" {
   project = var.project_id
   role    = "roles/composer.ServiceAgentV2Ext"
   member  = "serviceAccount:service-${data.google_project.current.number}@cloudcomposer-accounts.iam.gserviceaccount.com"
 }
 
-// Permet à Terraform d’agir AS dataloader-sa
-resource "google_service_account_iam_member" "dataloader_sa_act_as" {
+##############################
+# Permet à Composer d’agir AS dataloader-sa
+##############################
+resource "google_service_account_iam_member" "dataloader_sa_act_as_by_composer" {
+  service_account_id = google_service_account.dataloader_sa.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:service-${data.google_project.current.number}@cloudcomposer-accounts.iam.gserviceaccount.com"
+}
+
+##############################
+# IAM bindings pour TON user/SA Terraform
+# -> BIGQUERY : création de tables
+# -> STORAGE  : création d’objets
+# -> CLOUD FUNCTIONS : déploiement
+# -> CLOUD RUN       : déploiement
+# -> PUBSUB           : admin
+##############################
+
+resource "google_project_iam_member" "tf_bigquery_admin" {
+  project = var.project_id
+  role    = "roles/bigquery.admin"
+  member  = "user:${var.terraform_sa_email}"
+}
+
+resource "google_project_iam_member" "tf_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "user:${var.terraform_sa_email}"
+}
+
+resource "google_project_iam_member" "tf_cloudfunctions_admin" {
+  project = var.project_id
+  role    = "roles/cloudfunctions.admin"
+  member  = "user:${var.terraform_sa_email}"
+}
+
+resource "google_project_iam_member" "tf_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "user:${var.terraform_sa_email}"
+}
+
+resource "google_project_iam_member" "tf_pubsub_admin" {
+  project = var.project_id
+  role    = "roles/pubsub.admin"
+  member  = "user:${var.terraform_sa_email}"
+}
+
+##############################
+# Permet à TON Terraform d’agir AS dataloader-sa
+##############################
+resource "google_service_account_iam_member" "dataloader_sa_act_as_by_tf" {
   service_account_id = google_service_account.dataloader_sa.name
   role               = "roles/iam.serviceAccountUser"
   member             = "user:${var.terraform_sa_email}"
-}
-
-// Permet à Terraform de créer des tables BigQuery dans le dataset
-resource "google_bigquery_dataset_iam_member" "tf_bigquery_tables" {
-  dataset_id = var.bq_dataset_id
-  role       = "roles/bigquery.dataOwner"
-  member     = "user:${var.terraform_sa_email}"
-}
-
-// Permet à Terraform de gérer les fonctions Cloud Functions
-resource "google_project_iam_member" "tf_cloudfunctions_create" {
-  project = var.project_id
-  role    = "roles/cloudfunctions.developer"
-  member  = "user:${var.terraform_sa_email}"
 }
