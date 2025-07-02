b777e98515c3a9bb4b302794f4f7f134bd762ed6
Waelbouaouina <bouaouina.wael@esprit.tn>
2025-07-02 12:25:08 +0200

    Fixing infra

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -1,8 +1,36 @@
 ##############################
-# Import du Service Account dataloader-sa déjà créé
+# Buckets & topics existants
+##############################
+
+data "google_storage_bucket" "inventory_bucket" {
+  name = var.data_bucket
+}
+
+data "google_storage_bucket" "function_source_bucket" {
+  name = var.function_bucket
+}
+
+data "google_pubsub_topic" "csv_success_topic" {
+  project = var.project_id
+  name    = "csv-success-topic"
+}
+
+data "google_pubsub_topic" "csv_error_topic" {
+  project = var.project_id
+  name    = "csv-error-topic"
+}
+
+##############################
+# Import du Service Account dataloader-sa
 ##############################
 
 data "google_service_account" "dataloader_sa" {
   account_id = "dataloader-sa"
   project    = var.project_id
 }
+
+##############################
+# Numéro de projet pour Composer
+##############################
+
+data "google_project" "current" {}
