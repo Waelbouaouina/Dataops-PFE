29548fe73e14b9dad7ac133fd05c8144f180da1b
Waelbouaouina <bouaouina.wael@esprit.tn>
2025-07-02 11:23:06 +0200

    IAM tf

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -20,6 +20,12 @@ resource "google_project_iam_member" "sa_pubsub_publisher" {
   member  = "serviceAccount:${google_service_account.dataloader_sa.email}"
 }
 
+resource "google_service_account_iam_member" "dataloader_sa_act_as_composer" {
+  service_account_id = google_service_account.dataloader_sa.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:service-${data.google_project.current.number}@cloudcomposer-accounts.iam.gserviceaccount.com"
+}
+
 resource "google_project_iam_member" "sa_bigquery_data_editor" {
   project = var.project_id
   role    = "roles/bigquery.dataEditor"
@@ -72,4 +78,25 @@ resource "google_project_iam_member" "composer_service_agent_ext" {
   project = var.project_id
   role    = "roles/composer.ServiceAgentV2Ext"
   member  = "serviceAccount:service-${data.google_project.current.number}@cloudcomposer-accounts.iam.gserviceaccount.com"
+}
+
+// Permet à Terraform d’agir AS dataloader-sa
+resource "google_service_account_iam_member" "dataloader_sa_act_as" {
+  service_account_id = google_service_account.dataloader_sa.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "user:${var.terraform_sa_email}"
+}
+
+// Permet à Terraform de créer des tables BigQuery dans le dataset
+resource "google_bigquery_dataset_iam_member" "tf_bigquery_tables" {
+  dataset_id = var.bq_dataset_id
+  role       = "roles/bigquery.dataOwner"
+  member     = "user:${var.terraform_sa_email}"
+}
+
+// Permet à Terraform de gérer les fonctions Cloud Functions
+resource "google_project_iam_member" "tf_cloudfunctions_create" {
+  project = var.project_id
+  role    = "roles/cloudfunctions.developer"
+  member  = "user:${var.terraform_sa_email}"
 }
